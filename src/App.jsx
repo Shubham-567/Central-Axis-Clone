7ccdade6c67d7784521f01f716b27559e8d0ddfb
Shubham <shubhampatil1356@gmail.com>
2025-05-17 00:36:36 +0530

    Initial setup complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-purple text-6xl text-center m-20 font-bold'>
+        Hello Shubham
+      </h1>
+    </div>
+  );
+};
+
+export default App;
